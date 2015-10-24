873d963c51222b09da03fbbde0c8d757142945b2
Andrew Du <aadu@ucsd.edu>
2015-10-24 07:38:48 -0700

    heroku

--- app.js
+++ app.js
@@ -5,12 +5,6 @@ var handlebars = require('express-handlebars');
 var app = express();
 var http = require('http').Server(app);
 
-var accountSid = 'ACe7874db6c92c30d733ccf07310448613'; 
-var authToken = '3c2263656c5efd649c7dd21158439c6c'; 
-
-//require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
-
 //Configures the Template engine
 app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
@@ -23,7 +17,7 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
 // Routes
-require('./routes/routes')(app, client);
+require('./routes/routes')(app);
 
 http.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
